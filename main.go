d94c52e2ad0144403842611dce9222f8d91a98c0
akorzunin <akorzunin123@gmail.com>
2023-12-26 15:20:14 +0300

    feat(day1): solved part 2

--- main.go
+++ main.go
@@ -18,7 +18,20 @@ func sum(array []int) int {
 	return result
 }
 
+type DigitArr = [10]string
+
+func getIndex(arr DigitArr, index string) int {
+	predicate := func(i int) bool { return arr[i] == index }
+	for i := 0; i < len(arr); i++ {
+		if predicate(i) {
+			return i
+		}
+	}
+	return -1
+}
+
 func main() {
+	digits := DigitArr{"_", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 	file, err := os.Open("./in.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -33,9 +46,17 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		var sub_arr []string
-		for _, v := range line {
+		for i, v := range line {
+			cutted_line := line[i:]
 			if unicode.IsDigit(v) {
 				sub_arr = append(sub_arr, string(v))
+			}
+			for _, digit := range digits {
+				if strings.HasPrefix(cutted_line, digit) {
+					parsed_digit := getIndex(digits, digit)
+					sub_arr = append(sub_arr, strconv.Itoa(parsed_digit))
+				}
+
 			}
 		}
 		if len(sub_arr) == 0 {
