9f82d2f9e193298dc5935307060b6f0b9603da0b
akorzunin <akorzunin123@gmail.com>
2024-12-01 20:25:16 +0300

    fix: upd aoc script

--- aoc.py
+++ aoc.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import subprocess
 import argparse
@@ -5,7 +6,7 @@ from pathlib import Path
 
 TOKEN = os.getenv(
     "TOKEN",
-    open(Path(os.getcwd()) / ".env", "r").read().strip(),
+    open(Path(os.getcwd()) / ".env", "r").read().strip().split("=")[-1],
 )
 # Parse the command-line arguments
 parser = argparse.ArgumentParser(description="Download Advent of Code data")
@@ -14,29 +15,33 @@ parser.add_argument(
     "--year",
     type=int,
     help="The year for the Advent of Code event",
+    default=datetime.datetime.now().year,
 )
 parser.add_argument(
     "-d",
     "--day",
     type=int,
     help="The day for which to download the data",
+    default=datetime.datetime.now().day,
+)
+parser.add_argument(
+    "-o",
+    "--out",
+    type=str,
+    help="The output file to write the data to",
+    default="in.txt",
 )
 args = parser.parse_args()
-
-# # Set the year and day
-year = args.year or 2023
-day = args.day or int(Path(os.getcwd()).stem[-1])
 
 if __name__ == "__main__":
     res = subprocess.run(
         [
             "curl",
-            f"https://adventofcode.com/{year}/day/{day}/input",
+            f"https://adventofcode.com/{args.year}/day/{args.day}/input",
             "-s",
             "--cookie",
             f"session={TOKEN}",
             "-o",
-            "in.txt",
+            args.out,
         ]
     )
-    print(TOKEN)
